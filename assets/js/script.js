252d51df2c5e4d36c0f2753de430dbef0a7682a0
Will <wdputnam42@gmail.com>
2023-04-18 17:56:42 -0400

    added an image and a submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log('hi')
+console.log('hello');
+var pokemonInputBox = document.getElementById('pokemon-finder');
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    console.log(data.name);
+    console.log(data.abilities);
+})
