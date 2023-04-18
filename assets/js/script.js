90f7e3acd36d85bc88980a9e3fd607ea1b3871ff
Will <wdputnam42@gmail.com>
2023-04-18 19:22:56 -0400

    its working now

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,29 @@ pokemonButton.addEventListener('click', function (event) {
     event.preventDefault();
     // console.log(pokemonInputBox.value);
     var userChoice = pokemonInputBox.value.toLowerCase();
-    fetch("https://pokeapi.co/api/v2/pokemon/" + userChoice)
+    getPokemonData(userChoice);
+    
+    console.log('hi');
+})
+
+function getPokemonData(pokemonName) {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
             console.log(data.name);
-            console.log(data.abilities);
+            
             var cardEl = document.createElement('div');
+            cardEl.setAttribute("class", "card col-md-3");
+            cardEl.setAttribute("style", "width: 20rem");
             cardEl.textContent = data.name;
+            for(var i = 0; i < data.moves.length; i++) {
+               var moveNames = document.createElement('div');
+               moveNames.textContent = data.moves[i].move.name;
+               cardEl.appendChild(moveNames); 
+            }
             pokemonBox.appendChild(cardEl);
         })
-    console.log('hi');
-})
+}
